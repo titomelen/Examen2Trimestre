bf6c2a53bcfbbab19629c73049f082f9007472a6
titomelen <javiermelendez744@gmail.com>
2020-03-26 14:16:54 +0100

    #3 Subo la funcion 2 de nuevo, estaba mal

--- Funciones.java
+++ Funciones.java
@@ -16,3 +16,29 @@ public class funcion1 {
 			return resp;
 		};
 }
+public class funcion2 {
+
+  public static void main(String[] args) {
+
+    System.out.println(Funcion2(153));
+  }
+
+  public static Boolean funcion2(int num) {
+
+    Boolean resp = null;
+
+    int y = 200 - 50;
+    int x = 200 + 50;
+
+    if (num <= x && num >= y) {
+
+      resp = true;
+
+    } else {
+      resp = false;
+    }
+
+    return resp;
+
+  }
+}
