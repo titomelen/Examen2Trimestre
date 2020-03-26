909c666311833b719ba2fdb8845e79bbd068337a
titomelen <javiermelendez744@gmail.com>
2020-03-26 14:23:30 +0100

    #5 Subo la funcion 4

--- Funciones.java
+++ Funciones.java
@@ -64,3 +64,26 @@ public class funcion3 {
     }
 
 }
+public class funcion4 {
+        public static void main(String[] args) {
+
+
+                String[] CadenaNombres = { "Javier Melendez", "Carlos Garcia", "Carlos Villar", "Juan Jesus" };
+
+                System.out.println(funcion4(CadenaNombres));
+
+        }
+                public static String funcion4(String[] CadenaNombres) {
+
+                        String CadenaConcatenada = null;
+                        for (int i = 0; i < CadenaNombres.length; i++) {
+
+                                CadenaConcatenada = CadenaNombres[0] + "-" + CadenaNombres[1] + "-" + CadenaNombres[2] + "-"
+                                                + CadenaNombres[3];
+                        }
+                        CadenaConcatenada = CadenaConcatenada.replaceAll("[AaEeIiOoUu]", "");
+                        CadenaConcatenada = CadenaConcatenada.replaceAll("[A-Z]", "");
+                        System.out.println(CadenaConcatenada);
+                        return CadenaConcatenada;
+                }
+}
